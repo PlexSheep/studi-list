b1120621172294b736062fe3f859488564d4d6c2
0xalivecow <10931446-0xalivecow@users.noreply.gitlab.com>
2022-11-28 10:14:31 +0100

    main main menu added

--- main.c
+++ main.c
@@ -135,5 +135,46 @@ int deleteStudent(int mNum){
 }
 
 int main(){
+    int ende =0;
+    int wahl;
+
+    printf("(1) Neuen Studenten anlegen\n");
+    printf("(2) Anzahl der gespeicherten Studenten abrufen\n");
+    printf("(3) Student anhand der Martikelnummer ausgeben\n");
+    printf("(4) Alle Studenten alphabetisch ausgeben\n");
+    printf("(5) Studenten loeschen\n");
+    printf("(6) Programm beenden\n");
+
+    do{
+        printf("Bitte Wahl treffen: ");
+        char tmpnum[50];
+        fgets(tmpnum, 50, stdin);
+        wahl = atoi(tmpnum);
+
+        switch(wahl){
+            case 1:
+                addStudent();
+                break;
+            case 2:
+                anzahl();
+                break;
+            case 3:
+                printStudent();
+                break;
+            case 4:
+                printAllStudents();
+                break;
+            case 5:
+                deleteStudent();
+                break;
+            case 6:
+                ende=1;
+                break;
+            default:
+                printf("Bitte einen gueltigen Wert eingeben");
+        }
+    }while (ende!=1);
+    printf("Auf Wiedersehen");
+    return 0;
     return 0;
 }
