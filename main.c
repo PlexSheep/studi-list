9d99740a17ef8a26116adef02e7b736acf0b00f5
PlexSheep <PlexSheep@protonmail.com>
2022-11-28 08:54:52 +0100

    main.c addStudent()

--- main.c
+++ main.c
@@ -17,12 +17,18 @@ struct student{
 
 // make a global var to store all of our students. This is a pointer because we want to use malloc()
 struct student* studentArr;
+int studentArrLength = 0;
 
 struct student inputStudent(){
 
     struct student s;
 
     return s;
+}
+
+void addStudent(struct student* s){
+    // TODO check if given student is already in the arr
+   studentArr[studentArrLength++ + 1] = *s;
 }
 
 int main(){
